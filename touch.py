337a8747283c9df83d8babea4114b8a7f3b77513
Polovinkin Nikita <poloniki@ya.ru>
2022-11-28 09:39:43 +0100

    Test

--- touch.py
+++ touch.py
@@ -1,4 +1,7 @@
 import random
-
-def predict():
-    return random.randint(1,10)
+import pandas
+def test():
+    def predict(imt):
+        print('new')
+        return random.randint(1,10)
+    return Error
